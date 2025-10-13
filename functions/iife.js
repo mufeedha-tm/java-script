d413576ced2ae7102a4a926dc93490e6c11b8901
mufeedha-tm <mufeedha059@gmail.com>
2025-10-13 11:16:19 +0530

    first commit

--- functions/iife.js
+++ functions/iife.js
@@ -1,8 +1,10 @@
 //immediately invoked function expression
+//it is a function that runs as soon as it is defined,without need to be called separately
 //---key features---
 // 1) immediately invoked
 // 2) data privacy-variables defined inside iife not accessible outside.
-// 3) closure
+// 3) closure-A closure lets a function access variables from an outer fuction,even after the outer function is done.
+
 // 4) self contained-execute independantly without effecting surrounding code.
 
 (function(){
